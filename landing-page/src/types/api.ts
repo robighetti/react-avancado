6e79cf67d9d2cb8e6559b2e281208628b5fe5ce2
Rodrigo Bighetti <rodrigo.bighetti@avanade.com>
2021-01-18 18:27:56 -0300

    first upload

---
+++ landing-page/src/types/api.ts
@@ -0,0 +1,128 @@
+export type Image = {
+  alternativeText: string
+  url: string
+}
+
+export type Module = {
+  title: string
+  subtitle: string
+  description: string
+}
+
+export type TechIcon = {
+  title: string
+  icon: {
+    url: string
+  }
+}
+
+export type Concept = {
+  title: string
+}
+
+export type LogoProps = {
+  alternativeText: string
+  url: string
+}
+
+export type headerProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: Image
+}
+
+export type SectionAboutProjectProps = {
+  title: string
+  description: string
+  image: Image
+}
+
+export type SectionTechProps = {
+  title: string
+  techIcons: TechIcon[]
+}
+
+export type SectionConceptsProps = {
+  title: string
+  concepts: Concept[]
+}
+
+export type SectionModulesProps = {
+  title: string
+  modules: Module[]
+}
+
+export type SectionAgendaProps = {
+  title: string
+  description: string
+}
+
+export type PricingBoxProps = {
+  totalPrice: number
+  numberInstallments: number
+  priceInstallments: number
+  benefits: string
+  button: {
+    label: string
+    url: string
+  }
+}
+
+export type SocialLink = {
+  title: string
+  url: string
+}
+
+export type Author = {
+  photo: Image
+  name: string
+  role: string
+  socialLinks: SocialLink[]
+  description: string
+}
+
+export type SectionAboutUsProps = {
+  title: string
+  authors: Author[]
+}
+
+export type Review = {
+  name: string
+  text: string
+  photo: {
+    url: string
+  }
+}
+
+export type SectionReviewsProps = {
+  title: string
+  reviews: Review[]
+}
+
+export type Question = {
+  question: string
+  answer: string
+}
+
+export type SectionFaqProps = {
+  title: string
+  questions: Question[]
+}
+
+export type LandingPageProps = {
+  logo: LogoProps
+  header: headerProps
+  sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
+  sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
+  sectionAgenda: SectionAgendaProps
+  pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
+  sectionReviews: SectionReviewsProps
+  sectionFaq: SectionFaqProps
+}
---
+++ reactavancado-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'de5288e0-f923-494a-a633-b724843cef70'
+};
