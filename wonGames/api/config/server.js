6e011622ad439c3415c3d1b1e524560a51ce5d34
Rodrigo Bighetti <rodrigo.bighetti@avanade.com>
2021-01-18 22:54:32 -0300

    iniciando o desenvolvimento da api

---
+++ wonGames/api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd065f42eed4b9fe47b55269df806026e'),
+    },
+  },
+});
---
+++ wonGames/api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '30dacf1a-606f-4050-8ebe-234360e15da3'
+};
